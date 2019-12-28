52298a289f7f20504f8da2214772cf6e599f8de9
jantapabh <jantapa2407@gmail.com>
2019-12-28 18:04:37 +0700

    define port

---
+++ server.js
@@ -0,0 +1,25 @@
+const expressFunction = require('express');
+const expressApp = expressFunction();
+
+
+expressApp.get('/', function(req, res){
+    res.send('Hello World');
+
+});
+
+expressApp.get('/api/resource', function(req, res) {
+
+    var myJson = {
+        name: 'Jan',
+        id: 1
+    };
+
+    res.send(myJson);
+});
+
+const port = process.env.PORT || 4100;
+
+expressApp.listen(port, function(){
+
+    console.log('Listening on PORT 4100');
+});
