5ceaf780c5abf0d89444a0b28577da826b2b49f8
jantapabh <jantapa2407@gmail.com>
2019-12-28 21:20:11 +0700

    test2

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const port = process.env.PORT || 4100;
 
 expressApp.listen(port, function(){
 
-    console.log('Listening on PORT port');
+    console.log('Listening on PORT 4100');
 
 });
---
+++ test2.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const student = [ {
+    id: 1, name: "Jan"
+} , {
+
+    id: 2, name: "Fairee"
+}];
+
+app.get('/api/student', function(req,res){
+    res.send(student);
+});
+
+app.get('/api/student/:id',function(req, res){
+    const id = req.paeams.id;
+
+    if(id == 1){
+        res.send(student[0]);
+
+    } else if(id == 2){
+        res.send(student[1]);
+    }else{
+        res.send('Error 404 not found');
+
+    }
+
+
+});
+
+const port = process.env.PORT || 3000;
+app.listen(3000, function(){
+    console.log('Listenning on PORT 3000');
+});
